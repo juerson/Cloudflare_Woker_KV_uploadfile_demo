9ac972b08857761ba27c10e9d4c171e223506a16
unknown <zhangrson@163.com>
2024-03-26 16:38:34 +0800

    update

--- README.md
+++ README.md
@@ -2,36 +2,44 @@ Cloudflare Workers KV 练手Demo，通过上传文件，将文件中的所有内
 
 ### 操作步骤：
 
-1、在Workers KV创建一个命名空间名称，名称随意。比如，创建为“uploadfile”的命名空间名称
+1、在Workers KV创建一个命名空间名称，名称随意。比如，创建为“uploadfile”的命名空间名称。
 
-<img src="images\Snipaste_2024-03-26_00-30-15.png" />
+<img src="images\创建KV.png" />
 
-2、创建Worker，名称随意，然后部署。
+2、KV中，添加用户名、密码。
 
-<img src="images\Snipaste_2024-03-26_00-14-23.png" />
+<img src="images\KV中，添加用户名和密码.png" />
 
-3、在worker项目中，绑定创建的KV命名空间：
+3、创建Worker，名称随意，然后部署。
 
-​	  (设置 -> 变量 -> KV 命名空间绑定 -> 添加绑定)
+<img src="images\创建worker脚本.png" />
 
-<img src="images\Snipaste_2024-03-25_23-24-04.png" />
+4、在worker项目中，绑定创建的KV命名空间：
+
+​	  (操作步骤：设置 -> 变量 -> KV 命名空间绑定 -> 添加绑定)
+
+<img src="images\绑定KV命令空间.png" />
 
 - KV变量跟KV命名空间的关系：
 
-<img src="images\Snipaste_2024-03-25_23-27-30.png" />
+<img src="images\绑定的KV命名空间的变量名称跟KV命名空间的关系.png" />
 
-4、编辑代码（刚才创建的哪个worker项目），将index.js的所有代码复制到worker中保存。（这个步骤可以跟第2步骤一起做）
+5、编辑代码（刚才创建的哪个worker项目），将index.js的所有代码复制到worker中保存。（这个步骤可以跟第2步骤一起做）
 
-- 代码中的UPLOADKV与Worker项目中绑定的KV命令空间的变量名称有什么关系？
+- 代码中的UPLOADKV与Worker项目中KV变量名称有什么关系？
 
-<img src="images\Snipaste_2024-03-25_23-39-13.png" />
+<img src="images\代码中的UPLOADKV与Worker项目中KV变量名称有什么关系？.png" />
 
-5、设置 -> 触发器 -> 路由，下面有个网址（当然你也可以绑定自定义域，也就是自己的域名），点这个进去，就能打开刚才部署的worker项目，如下：
+- 代码put函数与KV密钥和值关系图
 
-<img src="images\Snipaste_2024-03-25_23-45-06.png" />
+<img src="images\代码put函数与KV密钥和值关系图.png" />
+
+6、设置 -> 触发器 -> 路由，下面有个网址（当然你也可以绑定自定义域，也就是自己的域名），点这个进去，就能打开刚才部署的worker项目，如下：
+
+<img src="images\网页运行效果.png" />
 
 - 这里上传的文件数据写到哪里去了?
 
-<img src="images\Snipaste_2024-03-26_00-01-39.png" />
+<img src="images\代码put函数与KV密钥和值关系图.png" />
 
 代码参考：https://developers.cloudflare.com/kv/reference/kv-bindings/
--- index.js
+++ index.js
@@ -4,25 +4,41 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
   let uploadMessage = "";
+  let KV_key = "config_template"; // 默认KV密钥名称
+  const user = await UPLOADKV.get('username'); // 获取KV中设置的用户名
+  const pwd = await UPLOADKV.get('password'); // 获取KV中设置的密码
   if (request.method === 'POST') {
     const formData = await request.formData();
-    const file = formData.get('file');
-    if (file) {
+    const file = formData.get('file'); // 获取表单中文件域
+    const username = formData.get('username'); // 获取表单中用户名
+    const password = formData.get('password'); // 获取表单中密码
+    const key_value = formData.get('key').trim(); // 获取表单中key
+    if (key_value && file && username === user && password === pwd && !["username", "password"].includes(key_value)) {
+      KV_key = key_value; // 更新KV密钥名称
       const text = await file.text();
-      await UPLOADKV.put('config_template', text); // UPLOADKV不存在就添加，存在就覆盖（设置->变量->KV 命名空间绑定->变量名称）
-      uploadMessage = `${file.name}文件内容上传成功！`;
+      await UPLOADKV.put(KV_key, text.trim()); // UPLOADKV不存在就添加，存在就覆盖（设置->变量->KV 命名空间绑定->变量名称）
+      uploadMessage = `${file.name}文件的内容写入KV变量中！`;
     }
   }
-  let config_template = await UPLOADKV.get('config_template'); // 获取KV中config_template变量的值
+  let KV_key_Value = await UPLOADKV.get(KV_key); // 获取KV中config_template密钥的值
   const html = `
-      <form action="./" method="POST" enctype="multipart/form-data" style="width: 327px; border: 1px solid #9d8f8f; padding: 8px;">
-        <input type="file" name="file">
+    <form action="./" method="POST" enctype="multipart/form-data" style="margin: auto auto; width: 462px; height:114px;border: 1px solid #9d8f8f; padding: 8px; line-height: 2.5em;">
+      <div style="width: 100%; display: flex; align-items: center; justify-content: space-between;">
+        <label>用户名：<input type="text" name="username" required></label>
+        <label>密码： <input type="password" name="password" required></label>
+      </div>
+      <label>KV密钥：<input type="text" value="${KV_key}" name="key"></label>
+      <div style="width: 100%; display: flex; justify-content: space-between;">
+        <input type="file" name="file" required>
         <input type="submit" value="上传文件">
-      </form>
-      <p style="line-height: 14px;">${uploadMessage}</p>
-      <p style="line-height: 14px;">当前，KV中config_template变量对应的值：</p>
-      <textarea rows="42" cols="250">${config_template}</textarea>
-    `;
+      </div>
+    </form>
+    <div style="margin: auto auto; width:100%; text-align:center;">
+      <p style="line-height: 14px; color:red;"> ${uploadMessage} </p>
+      <p style="line-height: 14px;">当前，KV中${KV_key}密钥对应的值(会更新到最新上传的内容)：</p>
+      <textarea rows="36" cols="190">${KV_key_Value}</textarea>
+    </div>
+  `;
   return new Response(html, {
     headers: { 'content-type': 'text/html;charset=UTF-8' },
   })
